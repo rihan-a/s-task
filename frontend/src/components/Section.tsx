1f7d15793c527e69b5c1f0189641763673d3d42b
rihan-a <a.rihan@live.com>
2025-06-29 16:45:32 +0300

    update edit logic

--- frontend/src/components/Section.tsx
+++ frontend/src/components/Section.tsx
@@ -1,17 +1,21 @@
-import type {ComputedSectionType} from '../types';
+import type {ComputedSectionType, EntryType} from '../types';
 import SectionList from './SectionList';
 
 type Props = {
     node: ComputedSectionType;
+    onChange: (updated: ComputedSectionType | EntryType) => void;
 };
 
-export default function Section({node}: Props) {
+export default function Section({node, onChange}: Props) {
     return (
-        <div className=" ">
-            <div className="font-semibold">
+        <div className=" border border-black p-2 m-2 rounded-lg">
+            <div className="font-semibold text-left p-1">
                 {node.name} — {node.computedSum}
             </div>
-            <SectionList childrenNodes={node.children} />
+            <SectionList
+                childrenNodes={node.children}
+                onChildChange={onChange}
+            />
         </div>
     );
 }
