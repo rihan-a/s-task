07a63ca44fc17705294470d51d47f1b027ff5903
rihan-a <a.rihan@live.com>
2025-06-29 15:37:13 +0300

    render computed sum

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,28 @@
 import './App.css';
-import {useEffect} from 'react';
-import type {Section} from './types';
+import {useEffect, useState} from 'react';
+import type {Section, ComputedSection} from './types';
+import {computeComputedSection} from './utils/computeSum';
 
 function App() {
+    const [tree, setTree] = useState<ComputedSection | null>(null);
     useEffect(() => {
         fetch('http://localhost:8000/api/report')
             .then((res) => res.json())
             .then((data: Section) => {
+                const computed = computeComputedSection(data);
+                setTree(computed);
                 console.log(data);
             });
     }, []);
 
+    if (!tree) return <div>Loading...</div>;
+
     return (
         <div>
             <h1>Splinde Task</h1>
+            <h2>
+                {tree.name} — Total: {tree.computedSum}
+            </h2>
         </div>
     );
 }
