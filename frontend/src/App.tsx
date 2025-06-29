ccdb35048f19f8d29ac03f6825cb1b22674584cc
rihan-a <a.rihan@live.com>
2025-06-29 16:00:03 +0300

    update type names & render sections

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css';
 import {useEffect, useState} from 'react';
-import type {Section, ComputedSection} from './types';
+import type {SectionType, ComputedSectionType} from './types';
 import {computeComputedSection} from './utils/computeSum';
+import Section from './components/Section';
 
 function App() {
-    const [tree, setTree] = useState<ComputedSection | null>(null);
+    const [tree, setTree] = useState<ComputedSectionType | null>(null);
     useEffect(() => {
         fetch('http://localhost:8000/api/report')
             .then((res) => res.json())
-            .then((data: Section) => {
+            .then((data: SectionType) => {
                 const computed = computeComputedSection(data);
                 setTree(computed);
                 console.log(data);
@@ -20,9 +21,8 @@ function App() {
     return (
         <div>
             <h1>Splinde Task</h1>
-            <h2>
-                {tree.name} — Total: {tree.computedSum}
-            </h2>
+            <h2></h2>
+            <Section node={tree} />
         </div>
     );
 }
