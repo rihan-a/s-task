3e1f0264d771c8e5751547c94261f8320472e66c
rihan-a <a.rihan@live.com>
2025-06-29 15:22:40 +0300

    fetch api data in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
 import './App.css';
+import {useEffect} from 'react';
+import type {Section} from './types';
 
 function App() {
-    return <div>hello world!</div>;
+    useEffect(() => {
+        fetch('http://localhost:8000/api/report')
+            .then((res) => res.json())
+            .then((data: Section) => {
+                console.log(data);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Splinde Task</h1>
+        </div>
+    );
 }
 
 export default App;
